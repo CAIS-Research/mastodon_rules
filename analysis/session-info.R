bb9198852d9d9f0606f58fda3181685dd460b0e6
marcohhu <marco.waehner@uni-duesseldorf.de>
2024-04-16 11:13:26 +0200

    add session info

---
+++ analysis/session-info.R
@@ -0,0 +1,18 @@
+# R Session info
+
+sessionInfo()
+
+R.Version()
+
+# version: packages
+
+#[1] topicmodels_0.2-14        ldatuning_1.0.2           lda_1.4.2                 tidytext_0.4.1           
+#[5] quanteda.textstats_0.96.3 summarytools_1.0.1        lubridate_1.9.2           forcats_1.0.0            
+#[9] stringr_1.5.0             dplyr_1.1.2               purrr_1.0.1               readr_2.1.4              
+#[13] tidyr_1.3.0               tibble_3.2.1              ggplot2_3.4.2             tidyverse_2.0.0          
+#[17] quanteda_3.3.1            rio_0.5.29                cld3_1.5.0                readtext_0.82            
+#[21] rtoot_0.3.3  
+
+# RStudio IDE - Version
+
+# RStudio 2023.12.1
